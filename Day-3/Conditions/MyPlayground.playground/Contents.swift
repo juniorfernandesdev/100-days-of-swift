dfd3fac125d5dc88d0143129a18e40a3ffa3b1e5
Junior Fernandes <juniorbass8@gmail.com>
2021-01-21 17:42:47 -0300

    Conditions and Combining conditions

---
+++ Day-3/Conditions/MyPlayground.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+let firstCard = 11
+let secondCard = 10
+
+if firstCard + secondCard == 21 {
+    print("Blackjack!")
+}
+
+if firstCard + secondCard == 21 {
+    print("Blackjack!")
+} else {
+    print("Regular cards")
+}
+
+if firstCard + secondCard == 2 {
+    print("Aces - lucky!")
+} else if firstCard + secondCard == 21 {
+    print("Blackjack!")
+} else {
+    print("Regular cards")
+}
+
+
+//Combining conditions
+
+let age1 = 12
+let age2 = 21
+
+if age1 > 18 && age2 > 18 {
+    print("Both are over 18")
+}
+
+
+if age1 > 18 || age2 > 18 {
+    print("At least one is over 18")
+}
--- README.md
+++ README.md
@@ -32,8 +32,8 @@
 - [x] 2. Operator overloading
 - [x] 3. Compound assignment operators
 - [x] 4. Comparison operators
-- [ ] 5. Conditions
-- [ ] 6. Combining conditions
+- [x] 5. Conditions
+- [x] 6. Combining conditions
 - [ ] 7. The ternary operator
 - [ ] 8. Switch statements
 - [ ] 9. Range operators
